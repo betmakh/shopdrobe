42a84c587faa82579bc65bfa2f355f40c4c15b9a
Mykyta Meshcheriakov <6etmah@gmail.com>
2014-11-26 21:35:58 +0200

    1, 2 section + slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 $(function() {
+  var features = [];
+  $('.features .feature').click(function(event) {
+    $('.features .feature').removeClass('active')
+    $(this).addClass('active');
+    for(var i = 0; i < $('.features .feature').length; i++) {
+      console.log(i)
+      if($(this).is($('.features .feature')[i])) {
+        $(this).closest('section').find('.iphone').attr('class', 'iphone slide-' + (i+1));
+      }
+
+    }
+  });
+  // setTimeout(sliderInit, 1000);
+  // sliderInit();
   
+//   $('.features .feature').each(function(index, el) {
+//     features[index] = el;
+//     $(el).on('click', function () {
+//       $(this).closest('section').find('.iphone').attr('class', 'iphone slide-' + (++index));
+//       console.log(features);
+//       $('.features .feature').removeClass('active')
+//       $(this).addClass('active');
+//     }) 
+//   });
 });
 
+function sliderInit() {
+  for (var i = 0; i < $('.features .feature').length; i++ ) {
+    console.log('nuka '+ i);
+    $('.features .feature')[i].click(function () {
+      console.log($(this));
+      $(this).closest('section').find('.iphone').attr('class', 'iphone slide-' + (i+1));
+      $('.features .feature').removeClass('active')
+      $(this).addClass('active');
+    });
+  }
+}
 function hidePopUp() {
   $('.popup-wrapper').removeClass('active');
 }
