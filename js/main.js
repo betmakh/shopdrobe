c1ed5f6535f1f2ba0129f6dd926667e5701fd679
Mykyta Meshcheriakov <6etmah@gmail.com>
2014-11-29 14:19:25 +0200

    js cool sliding

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+
+
+  $(document).ready(function() {
+      
+});
+    
 $(function() {
   var features = [];
   $('.features .feature').click(function(event) {
@@ -9,6 +15,50 @@ $(function() {
         $(this).closest('section').find('.iphone').attr('class', 'iphone slide-' + (i+1));
       }
 
+    }
+
+    // $('#pagepiling').pagepiling({
+    //     // menu: null,
+    //     // direction: 'vertical',
+    //     // verticalCentered: true,
+    //     // sectionsColor: [],
+    //     // anchors: [],
+    //     // scrollingSpeed: 700,
+    //     // easing: 'swing',
+    //     // loopBottom: false,
+    //     // loopTop: false,
+    //     css3: true
+    //     // // navigation: {
+    //     // //     'textColor': '#000',
+    //     // //     'bulletsColor': '#000',
+    //     // //     'position': 'right',
+    //     // //     'tooltips': ['section1', 'section2', 'section3', 'section4']
+    //     // // },
+    //     // normalScrollElements: null,
+    //     // normalScrollElementTouchThreshold: 5,
+    //     // touchSensitivity: 5,
+    //     // keyboardScrolling: true,
+    //     // sectionSelector: 'section',
+    //     // animateAnchor: false,
+
+    //     //events
+    // });
+  });
+  $('#pagepiling').pagepiling({
+    menu: '#nav',
+    anchors: ['page1', 'page2', 'page3', 'page4'],
+    navigation: {
+      'textColor': '#f2f2f2',
+      'bulletsColor': '#ccc',
+      'position': 'right',
+      'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4']
+    },
+    afterLoad: function(anchorLink, index){
+      if(anchorLink == 'page1'){
+          $('#nav').find('.logo').css('opacity', '1');
+      } else {
+          $('#nav').find('.logo').css('opacity', '0');
+      }
     }
   });
   // setTimeout(sliderInit, 1000);
