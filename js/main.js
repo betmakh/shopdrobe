51d51e3d566e96d3d5757b7326f7b30c8467e3b6
Mykyta Meshcheriakov <6etmah@gmail.com>
2014-12-10 22:12:21 +0200

    disabled mobile slider click

--- js/main.js
+++ js/main.js
@@ -1,44 +1,22 @@
  
 $(function() {
   var features = [];
-  $('.features .feature').click(function(event) {
-    $('.features .feature').removeClass('active')
-    $(this).addClass('active');
-    for(var i = 0; i < $('.features .feature').length; i++) {
-      console.log(i)
-      if($(this).is($('.features .feature')[i])) {
-        $(this).closest('section').find('.iphone').attr('class', 'iphone slide-' + (i+1));
+
+  if(!isMobile()) {
+    $('.features .feature').click(function(event) {
+      $('.features .feature').removeClass('active')
+      $(this).addClass('active');
+      for(var i = 0; i < $('.features .feature').length; i++) {
+        console.log(i)
+        if($(this).is($('.features .feature')[i])) {
+          $(this).closest('section').find('.iphone').attr('class', 'iphone slide-' + (i+1));
+        }
+
       }
+    });
+  }
+  
 
-    }
-
-    // $('#pagepiling').pagepiling({
-    //     // menu: null,
-    //     // direction: 'vertical',
-    //     // verticalCentered: true,
-    //     // sectionsColor: [],
-    //     // anchors: [],
-    //     // scrollingSpeed: 700,
-    //     // easing: 'swing',
-    //     // loopBottom: false,
-    //     // loopTop: false,
-    //     css3: true
-    //     // // navigation: {
-    //     // //     'textColor': '#000',
-    //     // //     'bulletsColor': '#000',
-    //     // //     'position': 'right',
-    //     // //     'tooltips': ['section1', 'section2', 'section3', 'section4']
-    //     // // },
-    //     // normalScrollElements: null,
-    //     // normalScrollElementTouchThreshold: 5,
-    //     // touchSensitivity: 5,
-    //     // keyboardScrolling: true,
-    //     // sectionSelector: 'section',
-    //     // animateAnchor: false,
-
-    //     //events
-    // });
-  });
   $('#pagepiling').pagepiling({
     menu: '#nav',
     anchors: ['first', 'second', 'third', 'fourth'],
@@ -63,4 +41,3 @@ $(function() {
 function hidePopUp() {
   $('.popup-wrapper').removeClass('active');
 }
-
